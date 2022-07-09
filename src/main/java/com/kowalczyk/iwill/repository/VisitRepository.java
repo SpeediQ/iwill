77b2fa50a07f2239daebcd696d848e1b7f437280
Marcin Kowalczyk <kowalczyk.m@int.pl>
2022-07-09 02:28:20 +0200

    Code for ServiceType manager has been prepared

--- src/main/java/com/kowalczyk/iwill/repository/VisitRepository.java
+++ src/main/java/com/kowalczyk/iwill/repository/VisitRepository.java
@@ -3,6 +3,12 @@ package com.kowalczyk.iwill.repository;
 
 import com.kowalczyk.iwill.model.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VisitRepository extends JpaRepository<Visit, Integer> {
+
+    @Query("select v from Visit v where v.status.id = ?1")
+    List <Visit> getAllReservationsByStatus(Integer id);
 }
